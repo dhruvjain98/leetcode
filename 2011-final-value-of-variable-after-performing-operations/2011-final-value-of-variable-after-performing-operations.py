2a817244d177e8af9a40cf6ba8e81f49a075dc5b
Dhruv Jain <43451472+DhruvJain1@users.noreply.github.com>
2022-07-04 19:00:45 +0530

    Time: 99 ms (20.88%), Space: 13.9 MB (11.94%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -3,6 +3,12 @@ class Solution:
         
         val = 0
         
+        # for item in operations:
+        #     if item[0] == "-" or item[1] == "-":
+        #         val -= 1
+        #     if item[0] == "+" or item[1] == "+":
+        #         val += 1
+        
         for item in operations:
             if '+' in item:
                 val += 1
