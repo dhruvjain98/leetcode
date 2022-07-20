5266b549b211a95c357a0236ef4a7dc64a0d613b
Dhruv Jain <43451472+DhruvJain1@users.noreply.github.com>
2022-07-20 23:28:42 +0530

    Time: 113 ms (49.52%), Space: 15.1 MB (65.27%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -6,12 +6,9 @@ class Solution:
         #             return ([i,j])
         
         prevMap = {} # val: index
+        
         for i,n in enumerate(nums):
             diff = target - n
             if diff in prevMap:
                 return [prevMap[diff], i]
-            prevMap[n] = i
-        return
-                
-                
-        
+            prevMap[n] = i        
