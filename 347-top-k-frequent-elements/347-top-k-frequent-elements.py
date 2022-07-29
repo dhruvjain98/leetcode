8ee6ae2269055e2948a9797c63d7d77dc63784b8
Dhruv Jain <43451472+DhruvJain1@users.noreply.github.com>
2022-07-30 03:41:30 +0530

    Time: 2158 ms (5.02%), Space: 18.6 MB (71.01%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,8 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        # sortNums = sorted(nums)
-        
+ 
         numsSet = set(nums)
         numsSetList = list(numsSet)
         
@@ -10,15 +8,6 @@ class Solution:
         for i in numsSetList:
             res[i] = nums.count(i)
         
-        print(res)
-        
-        # resList = list()
-        
-        # for key,val in res.items():
-        #     resList.append(key)
-        
         resList = sorted(res, key=res.get, reverse = True)
-            
-        # print(resList)
         
         return (resList[0:k])
