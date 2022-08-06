f5445d32bc1ced663f8c7e450b20f87a63c3e5de
Dhruv Jain <43451472+DhruvJain1@users.noreply.github.com>
2022-08-06 19:32:40 +0530

    Time: 153 ms (60.61%), Space: 19.6 MB (15.05%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,15 +1,38 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
- 
-        numsSet = set(nums)
-        numsSetList = list(numsSet)
+# 
+#         numsSet = set(nums)
+#         numsSetList = list(numsSet)
         
-        res = dict()    # num : count
-        for i in numsSetList:
-            res[i] = nums.count(i)
+#         res = dict()    # num : count
+#         for i in numsSetList:
+#             res[i] = nums.count(i)
             
-        # print(res.get(-1))
+#         # print(res.get(-1))
         
-        resList = sorted(res, key=res.get, reverse = True)
+#         resList = sorted(res, key=res.get, reverse = True)
         
-        return (resList[0:k])
+#         return (resList[0:k])
+
+        # BucketSort
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        res = []
+        
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        # print(count)
+        
+        for n,c in count.items():
+            freq[c].append(n)
+        # print(freq)
+        
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return(res)
+        
+        
+        
+        
