09c04101c87d994e63b5918736c5ff4954688392
Dhruv Jain <43451472+DhruvJain1@users.noreply.github.com>
2022-08-11 00:55:30 +0530

    Time: 91 ms (68.70%), Space: 14.4 MB (18.39%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -2,30 +2,28 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         
         stack = []
-        
         for i in tokens:
-            # print(i)
-            
             try:
                 num = int(i)
                 stack.append(int(i))
             except:
-                # print("Stack: {}: {}".format(stack, i))
                 v1 = stack.pop()
                 v2 = stack.pop()
+                
                 if i == "+": stack.append(v2 + v1)
                 if i == "-": stack.append(v2 - v1)
                 if i == "*": stack.append(v2 * v1)
+                
                 # if i == "/": stack.append(int(v2 // v1))
-                if i == "/":
+                
+                if i == "/":    # floor divide for negative numbers
                     if v1 < 0 or v2 < 0: stack.append(int(v2/v1))
                     else: stack.append(int(v2 // v1))
+        return stack[-1]
             
-            
-            
+            # To handle "-ve" numbers in the string. isnumeric checks if every char in string is numeric or not.
             # if i.isnumeric():
             #     stack.append(int(i))
-            
             # else:
             #     print("Stack: {}".format(stack))
             #     v1 = stack.pop()
@@ -34,5 +32,3 @@ class Solution:
             #     if i == "-": stack.append(v2 - v1)
             #     if i == "*": stack.append(v2 * v1)
             #     if i == "/": stack.append(v2 // v1)
-                    
-        return stack[-1]
