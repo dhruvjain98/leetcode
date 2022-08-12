239c9f35677789cc14c0f888e54ede3ba4615dc2
Dhruv Jain <43451472+DhruvJain1@users.noreply.github.com>
2022-08-12 23:40:03 +0530

    Time: 646 ms (17.93%), Space: 16.3 MB (80.79%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -18,12 +18,23 @@ class Solution:
         return count
         
     def dfs(self, grid, i, j):
+        
+        
+        # matx = [[-1,0], [1, 0], [0,-1], [0, 1]]
+        
         print("i, j: {}, {}".format(i, j))
         if i<0 or j<0 or i >= len(grid) or j >= len(grid[0]):
             return
         
         elif grid[i][j] == "1":
             grid[i][j] = "0"
+            
+            
+#             for x in range(len(matx)):
+#                 for y in range(len(matx[0])):
+#                     self.dfs(grid, i+x, j+y)
+            
+            
             self.dfs(grid, i+1, j)
             self.dfs(grid, i-1, j)
             self.dfs(grid, i, j+1)
