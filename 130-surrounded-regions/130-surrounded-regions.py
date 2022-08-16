9fef9470d32b5167e5e3359de5e4eb19db647b80
Dhruv Jain <43451472+DhruvJain1@users.noreply.github.com>
2022-08-16 18:28:19 +0530

    Time: 328 ms (14.20%), Space: 15.9 MB (51.86%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -0,0 +1,47 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
+        """
+        1. Convert unsurrounded regions to T    (O at edges of the board)
+        2. Convert surrounded regions to X
+        3. Convert T back to O
+        """
+        ROWS, COLS = len(board), len(board[0])
+        
+        def dfs(r, c):
+            if r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != "O":
+                return
+            
+            board[r][c] = "T"
+            # matx = [[1,0], [-1,0], [0,1], [0,-1]]
+            # for i in range(len(matx)):
+            #     for j in range(len(matx[0])):
+            #         dfs(r+i, c+j)
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+            
+        for r in range(ROWS):
+            for c in range(COLS):
+            
+                if board[r][c] == "O":
+                    if r in [0, ROWS - 1] or c in [0, COLS - 1]:
+                        dfs(r, c)
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "T":
+                    board[r][c] = "O"
+            
+            
+                
+            
